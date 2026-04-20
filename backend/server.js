ecb2cbdb03bf593463b8be6fd6a2de1d80bdb3ee
noa <noa.gomez.7e9@itb.cat>
2026-04-20 18:00:40 +0200

    fix: server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import cors from 'cors';
 
 import camisetasRouter from './routes/camisetas.routes.js';
@@ -9,9 +7,8 @@ import comandasRouter from './routes/comandas.routes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configuración para obtener el directorio actual en ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.use(express.json());
+app.use(cors());
 
 // Middlewares globales
 app.use(express.json());
@@ -26,18 +23,6 @@ app.use((req, res, next) => {
 // 1. RUTAS DE LA API
 app.use('/api/camisetas', camisetasRouter);
 app.use('/api/comandas', comandasRouter);
-
-// 2. SERVIR FRONTEND (Archivos estáticos)
-// Esto asume que 'frontend' está al mismo nivel que 'backend'
-app.use(express.static(path.join(__dirname, '../../frontend')));
-
-// 3. RUTA PARA EL INDEX.HTML (Single Page Application logic)
-app.get('/*catchall', (req, res) => {
-    // Si la petición no es para la API, enviamos el HTML
-    if (!req.url.startsWith('/api')) {
-        res.sendFile(path.join(__dirname, '../../frontend/html/index.html'));
-    }
-});
 
 // Middleware de errores
 app.use((err, req, res, next) => {
